b144d8574889eb1af33cf25f5b406f966ba04c91
Noel Mathew <noel.mathew007@gmail.com>
2020-10-11 12:47:49 +0000

    improv: resized images in dataset

--- src/dataset.py
+++ src/dataset.py
@@ -6,12 +6,13 @@ imagenet_stats = {'mean': [0.485, 0.456, 0.406], 'std': [0.229, 0.224, 0.225]}
 
 
 class Dataset:
-    def __init__(self, path, get_labels, get_image_files, label_enc):
+    def __init__(self, path, get_labels, get_image_files, label_enc, size=(700,300)):
         self.path = path
         self.image_files = get_image_files(self.path)  #  list of image file path
         self.labels = {p.stem: get_labels(p) for p in self.image_files}  #  dict of image file to label json dict
         self.label_enc = label_enc
         self.tfms = transforms.Compose([
+            transforms.Resize(size),
             transforms.ToTensor(),
             transforms.Normalize(**imagenet_stats)
         ])
